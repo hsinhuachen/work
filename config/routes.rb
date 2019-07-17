bfe05aee95a7b55b4b3704ffc71ab506aa1dc5ab
Hsinhua <yha0971@gmail.com>
2019-07-17 11:40:57 +0800

    20190717

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :projects, only: [:index, :show]
 
+  # resources :tags do
+  # 	resources :projects, only: [:index, :show]
+  # end
 
   # scope "/:locale" do
  	# root 'welcome#index'
 	# resources :projects, only: [:index, :show]
   # end
-
-  # get '/projects', to: 'projects#index'
-  # get '/projects/:id', to: 'projects#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
